9f22ad541e4c9983ff0346fb77fef3eb6ab83fd8
Aleix López <aleixlopeztrujillo@gmail.com>
2025-06-06 15:05:57 +0200

    adapt payments with handcash

--- back/src/functions/address.ts
+++ back/src/functions/address.ts
@@ -11,21 +11,30 @@
 
 import { PrivateKey } from '@bsv/sdk'
 import dotenv from 'dotenv'
+import { HandCashConnect } from '@handcash/handcash-connect';
 dotenv.config()
 
-const { NETWORK, FUNDING_WIF } = process.env
+const { NETWORK, FUNDING_WIF , WALLET_APP_ID , APP_SECRET } = process.env
+
+const handCashConnect = new HandCashConnect({
+  appId: WALLET_APP_ID,
+  appSecret: APP_SECRET,
+});
+
+const keyPair = handCashConnect.generateAuthenticationKeyPair();
 
 /**
  * Private key instance derived from WIF format
  * Used for signing transactions and deriving the public address
  */
-const key = PrivateKey.fromWif(FUNDING_WIF)
+//const key = PrivateKey.fromWif(FUNDING_WIF)
+const key = PrivateKey.fromString(keyPair.privateKey)
 
 /**
  * Bitcoin address derived from the private key
  * Uses different version byte (0x6f) for testnet addresses
  */
 //const address = NETWORK === 'test' ? key.toAddress([0x6f]) : key.toAddress()
-const address = '18KiFFTSSoBqcBP93Dx1tDBDjbuWoJzdVM'
+const address = NETWORK === 'main' ? '18KiFFTSSoBqcBP93Dx1tDBDjbuWoJzdVM' : ''
 
 export { key, address }
